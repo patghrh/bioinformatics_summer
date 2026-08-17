acfc36f52f2fec091cc147701f49457aef61095f
patghrh <patghrh@icloud.com>
2026-08-17 14:11:39 +0200

    Add fasta file input

--- python/scripts/analyze_fasta.py
+++ python/scripts/analyze_fasta.py
@@ -1,6 +1,15 @@
+import sys
 from fasta_parser import parse_fasta
-from sequence_analyzer import analyze_sequence
+from sequence_analyzer import analyze_sequence, print_report
 
-sequences = parse_fasta("data/raw/example.fasta")
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = input("Enter file path: ")
 
-print(sequences)
+sequences = parse_fasta(filename)
+
+for seq_id, sequence in sequences.items():
+    result = analyze_sequence(sequence)
+    print(seq_id)
+    print_report(result)
